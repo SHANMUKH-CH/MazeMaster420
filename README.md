1738e079fa9a1e0d09fd4dbf080f0a43779fb26e
TerrorBlade <37172194+SHANMUKH-CH@users.noreply.github.com>
2024-10-16 17:51:39 +0530

    dockerize the application (#1)
    Co-authored-by: TerrorBlade <shanmukhrodofatos@gmail.com>

--- README.md
+++ README.md
@@ -1,13 +1,23 @@
 # MazeMaster420
+
 A simple maze exploration game in Python.
+
+## Getting Started
+
+Clone the repository and set up the game:
 
 ```shell
 git clone --branch feat/game --single-branch https://github.com/SHANMUKH-CH/MazeMaster420.git
+cd MazeMaster420
 ./setup.sh
 ```
 
+## Requirements
+
+- Docker
+
 ## Access the Game
 
-After running the setup script, click the below link to access the game:
+After running the setup script, click the link below to access the game:
 
 [http://127.0.0.1:5000/](http://127.0.0.1:5000/)
--- setup.sh
+++ setup.sh
@@ -1,31 +1,7 @@
 #!/usr/bin/zsh
 
-# Check if Python is installed
-if ! command -v python3 &> /dev/null
-then
-    echo "Python3 is not installed. Installing Python3..."
-    sudo apt-get update
-    sudo apt-get install -y python3
-fi
+# Build the Docker image with the tag 'mazemaster420'
+docker build -t mazemaster420 .
 
-# Check if pip is installed
-if ! command -v pip3 &> /dev/null
-then
-    echo "pip3 is not installed. Installing pip3..."
-    sudo apt-get install -y python3-pip
-fi
-
-# Create a virtual environment
-python3 -m venv venv
-
-# Activate the virtual environment
-source venv/bin/activate
-
-# Upgrade pip to the latest version
-pip install --upgrade pip
-
-# Install Flask
-pip install flask
-
-# Run the Flask application
-python app.py
+# Run the Docker container, mapping port 5000 of the container to port 5000 on the host
+docker run -p 5000:5000 mazemaster420:latest
